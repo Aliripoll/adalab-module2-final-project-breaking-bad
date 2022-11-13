9b0867c9930d5213c4ed07cd42af4bc14277109b
Aliripoll <aripollortega@gmail.com>
2022-11-13 15:14:10 +0100

    add characters JSON

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-console.log('hola');
+// QUERYSELECTOR
+
+const charactersList = document.querySelector('.js_characters');
+let data = []; //listado del fetch
+
+// VARIABLES GLOBALES -> CON DATOS DE LA APP: personajes
+
+
+// FUNCIONES
+
+function renderCharacters() {
+  let html = "";
+  console.log(data);
+  for (const character of data) {
+    console.log(character);
+    html += `<img src="${character.img}"></>`; 
+    html += `<p> ${character.name}</p>`; 
+    html += `<p> ${character.status}</p>`; 
+
+}
+  charactersList.innerHTML = html;
+}
+
+fetch('https://breakingbadapi.com/api/characters')
+  .then((response) => response.json())
+  .then(characters => {
+    data = characters;
+    renderCharacters();
+  });
+
+
+// EVENTOS
